ddc84729899663fd4e66acbc60a7476da982a83e
Mkhotamirais <mkhotamirais@gmail.com>
2024-06-07 09:51:27 +0700

    selesai

--- client/src/app/api/apiSlice.js
+++ client/src/app/api/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({
     // baseUrl: `http://localhost:3000/api/warungota`,
-    baseUrl: `https://warung-ota-server.vercel.app/api/warungota`,
+    baseUrl: `https://mkhotami-warung-server.vercel.app/api/warungota`,
     credentials: "include",
   }),
   tagTypes: ["User", "Product", "Category", "Tag"],
